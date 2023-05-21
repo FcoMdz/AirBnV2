a40c371e3cfafcf93eb749189cc93cb16b6c1789
Gerardo12002 <fematdelgado.github@gmail.com>
2023-05-21 14:05:01 -0600

    Terminando registro con telefono

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
-import { getAuth, createUserWithEmailAndPassword, Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
-import { addDoc, collection, doc, getFirestore, setDoc } from '@angular/fire/firestore';
+import { getAuth, createUserWithEmailAndPassword, Auth, signInWithEmailAndPassword, RecaptchaVerifier, signInWithPhoneNumber, linkWithPhoneNumber, deleteUser } from '@angular/fire/auth';
+import { doc, getFirestore, setDoc, deleteDoc } from '@angular/fire/firestore';
 import { getDoc } from '@firebase/firestore';
 import { user } from 'src/app/services/local-storage.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { isValidPhoneNumber } from 'libphonenumber-js';
 
 @Component({
   selector: 'app-registro',
@@ -13,7 +14,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./registro.component.css'],
 })
 
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
 
   datosUsuario!:user;
 
@@ -26,25 +27,89 @@ export class RegistroComponent {
     usrName: new FormControl('', [Validators.required]),
     telefono: new FormControl('', [
       Validators.required,
-      Validators.minLength(10),
-      Validators.maxLength(10),
-      Validators.pattern('[0-9]*'),
+      this.PhoneNumberValidation()
     ]),
-    passwd: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    passwd: new FormControl('', [Validators.required, Validators.minLength(6)]),
     gender: new FormControl('', Validators.required),
   });
   sesion = new FormGroup({
     usrNameLog: new FormControl('', [Validators.required]),
     passwdLog: new FormControl('', [Validators.required]),
   });
+  phone = new FormGroup({
+    numberphone: new FormControl('',[
+      Validators.required,
+      this.PhoneNumberValidation()
+    ])
+  });
 
   passwdConf = new FormControl('', Validators.required);
   fondo = 'linear-gradient(135deg, #71b7e6, #9b59b6)';
 
   auth:Auth = getAuth();
+  appVerifier!:RecaptchaVerifier;
+  appVerifierRegistro!:RecaptchaVerifier;
 
   constructor(private router: Router, private route:ActivatedRoute){
+    this.auth.useDeviceLanguage();
+  }
 
+  ngOnInit(): void {
+      this.appVerifier = new RecaptchaVerifier('inputIniciarwphone',{
+        'size': 'invisible',
+        'callback': () => {
+          // reCAPTCHA solved, allow signInWithPhoneNumber.
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          Toast.fire({
+            icon: 'success',
+            title: 'Recaptcha confirmado'
+          });
+          this.loginwphone();
+        }
+      }, this.auth);
+      this.appVerifierRegistro =  new RecaptchaVerifier('inputRegistro',{
+        'size': 'invisible',
+        'callback': () => {
+          // reCAPTCHA solved, allow signInWithPhoneNumber.
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          Toast.fire({
+            icon: 'success',
+            title: 'Recaptcha confirmado'
+          }).then(()=>{
+            this.procesar();
+          });
+        }
+      }, this.auth);
+  }
+
+  PhoneNumberValidation(): ValidatorFn{
+    return (control: AbstractControl): ValidationErrors | null => {
+      let validNumber:boolean = false;
+      try{
+        validNumber = isValidPhoneNumber(control.value);
+      }catch(e){}
+      return validNumber ? null:{'wrongNumber':{value: control.value}};
+    }
   }
 
 
@@ -52,6 +117,7 @@ export class RegistroComponent {
     if (document.getElementById('registro')?.classList.contains('habilitado')) {
       let email:string = this.usuario.value.email || "";
       let passwd: string = this.usuario.value.passwd || "";
+      let phone: string = this.usuario.value.telefono || "";
       if((email || passwd) != ""){
         let button:HTMLInputElement = <HTMLInputElement>document.getElementById("inputRegistro");
         if(button){
@@ -66,25 +132,92 @@ export class RegistroComponent {
             const user = userCredential.user;
             const db = getFirestore();
             try{
-              const docRef = await setDoc(doc(db, 'usuarios', user.uid),{
+              await setDoc(doc(db, 'usuarios', user.uid),{
                 nombre: this.usuario.value.nombre,
                 email: this.usuario.value.email,
                 usrName: this.usuario.value.usrName,
                 telefono: this.usuario.value.telefono,
                 gender: this.usuario.value.gender,
               }).then(()=>{
-                Swal.fire(
-                  'Registro',
-                  'Se ha registrado correctamente, ' + this.usuario.value.nombre,
-                  'success'
-                ).then(()=>{
-                    this.clearForm();
-                    this.reactivarBoton(button, "Registrar");
-                    this.router.navigate(['home']);
-                  }
-                );
+                if(this.auth.currentUser){
+                  linkWithPhoneNumber(this.auth.currentUser, phone, this.appVerifierRegistro)
+                  .then((confirmResult)=>{
+                    Swal.fire({
+                      title: 'Confirma el código que recibiste por SMS',
+                      html: `<input type="number" id="codigo" class="swal2-input" placeholder="Codigo de confirmación">`,
+                      confirmButtonText: 'Confirmar',
+                      focusConfirm: false,
+                      preConfirm: () => {
+                        const popup = Swal.getPopup();
+                        if(popup){
+                          let recupera = <HTMLInputElement> popup.querySelector('#codigo');
+                          const login = recupera.value;
+                          if (!login) {
+                            Swal.showValidationMessage(`Introduce el código de confirmación`)
+                          }
+                          return { login: login }
+                        }
+                        return null;
+                      }
+                    }).then((result)=>{
+                      confirmResult.confirm(result.value?.login || "").
+                      then(()=>{
+                        Swal.fire(
+                          'Registro',
+                          'Se ha registrado correctamente, ' + this.usuario.value.nombre,
+                          'success'
+                        ).then(()=>{
+                            this.clearForm();
+                            this.reactivarBoton(button, "Registrar");
+                            this.router.navigate(['home']);
+                          }
+                        );
+                      })
+                      .catch((error)=>{
+                        if(this.auth.currentUser){
+                          deleteDoc(doc(db, 'usuarios', this.auth.currentUser.uid));
+                          deleteUser(this.auth.currentUser);
+                        }
+                        Swal.fire(
+                          'Registro',
+                          'Ha ocurrido un error: ' + error.message,
+                          'error').then(()=>{
+                            this.reactivarBoton(button, "Registrar");
+                          });
+                      });
+                    }).catch((error)=>{
+                      if(this.auth.currentUser){
+                        deleteDoc(doc(db, 'usuarios', this.auth.currentUser.uid));
+                        deleteUser(this.auth.currentUser);
+                      }
+                      Swal.fire(
+                        'Registro',
+                        'Ha ocurrido un error: ' + error.message,
+                        'error').then(()=>{
+                          this.reactivarBoton(button, "Registrar");
+                        });
+                    });
+                  })
+                  .catch((error)=>{
+                    if(this.auth.currentUser){
+                      deleteDoc(doc(db, 'usuarios', this.auth.currentUser.uid));
+                      deleteUser(this.auth.currentUser);
+                    }
+                    Swal.fire(
+                      'Registro',
+                      'Ha ocurrido un error: ' + error,
+                      'error'
+                    ).then(()=>{
+                      this.reactivarBoton(button, "Registrar");
+                    });
+                  });
+                }
               });
             }catch(error){
+              if(this.auth.currentUser){
+                deleteDoc(doc(db, 'usuarios', this.auth.currentUser.uid));
+                deleteUser(this.auth.currentUser);
+              }
               Swal.fire(
                 'Registro',
                 'Ha ocurrido un error: ' + error,
@@ -95,6 +228,7 @@ export class RegistroComponent {
             }
           })
           .catch((error) => {
+            console.log(error);
             Swal.fire(
               'Registro',
               'Ha ocurrido un error: ' + error.message,
@@ -114,7 +248,7 @@ export class RegistroComponent {
     if (document.getElementById('inicio')?.classList.contains('habilitado')) {
       let email:string = this.sesion.value.usrNameLog || "";
       let passwd: string = this.sesion.value.passwdLog || "";
-      if((email || passwd) != ""){
+      if((email && passwd) != ""){
         let button:HTMLInputElement = <HTMLInputElement>document.getElementById("inputIniciar");
         if(button){
           button.innerHTML = `
@@ -123,7 +257,6 @@ export class RegistroComponent {
           `;
           button.setAttribute("disabled","true");
         }
-        console.log(button.innerHTML);
         signInWithEmailAndPassword(this.auth, email, passwd)
           .then(async (userCredential)=>{
             const user = userCredential.user;
@@ -187,6 +320,79 @@ export class RegistroComponent {
     }
   }
 
+  loginwphone(){
+    if (document.getElementById('iniciowphone')?.classList.contains('habilitado')) {
+      let phone:string = this.phone.value.numberphone || "";
+      if((phone) != ""){
+        let button:HTMLInputElement = <HTMLInputElement>document.getElementById("inputIniciarwphone");
+        if(button){
+          button.innerHTML = `
+            <span class="spinner-border spinner-border-sm text-light" role="status" aria-hidden="true"></span>
+            Iniciando sesión...
+          `;
+          button.setAttribute("disabled","true");
+        }
+        signInWithPhoneNumber(this.auth, phone, this.appVerifier)
+          .then((confirmResult) => {
+            Swal.fire({
+              title: 'Confirma el código',
+              html: `<input type="number" id="codigo" class="swal2-input" placeholder="Codigo de confirmación">`,
+              confirmButtonText: 'Confirmar',
+              focusConfirm: false,
+              preConfirm: () => {
+                const popup = Swal.getPopup();
+                if(popup){
+                  let recupera = <HTMLInputElement> popup.querySelector('#codigo');
+                  const login = recupera.value;
+                  if (!login) {
+                    Swal.showValidationMessage(`Introduce el código de confirmación`)
+                  }
+                  return { login: login }
+                }
+                return null;
+              }
+            }).then((result) => {
+              if(result){
+                confirmResult.confirm(result.value?.login || "")
+                  .then((confirmacion)=>{
+                    Swal.fire(
+                      'Inicio de sesión',
+                      'Se ha iniciado correctamente, ' /*+ this.datosUsuario.nombre*/,
+                      'success'
+                    ).then(()=>{
+                        this.clearSesionwPhone();
+                        this.reactivarBoton(button, "Iniciar Sesión");
+                        this.router.navigate(['home']);
+                      }
+                    );
+                  })
+                  .catch((error)=>{
+                    Swal.fire(
+                      'Inicio de sesión',
+                      'Ha ocurrido un error: ' + error,
+                      'error').then(()=>{
+                        this.reactivarBoton(button, "Iniciar Sesión");
+                      });
+                  });
+              }
+            })
+          }).catch((error)=>{
+            Swal.fire(
+              'Inicio de sesión',
+              'Ha ocurrido un error: ' + error,
+              'error').then(()=>{
+                this.reactivarBoton(button, "Iniciar Sesión");
+              });
+          });
+      }else{
+        Swal.fire('Inicio de sesión', 'Verifique que los datos estén completos', 'error');
+      }
+
+    }else {
+      Swal.fire('Inicio de sesión', 'Verifique que los datos estén completos', 'error');
+    }
+  }
+
   reactivarBoton(button:HTMLInputElement, text:string){
     if(button){
       button.innerHTML = `
@@ -212,6 +418,12 @@ export class RegistroComponent {
     this.sesion.reset({
       usrNameLog: "",
       passwdLog: ""
+    })
+  }
+
+  clearSesionwPhone(){
+    this.phone.reset({
+      numberphone: '',
     })
   }
 
@@ -251,6 +463,10 @@ export class RegistroComponent {
       return true;
     }
     return false;
+  }
+
+  public get numberphone(){
+    return this.phone.get('numberphone');
   }
 
   /* //Configuracion para almacenamiento local
