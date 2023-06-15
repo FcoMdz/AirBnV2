d11637c65d2fbd39f12d68d902ef8da4310ef489
Gerardo12002 <fematdelgado.github@gmail.com>
2023-06-15 00:33:30 -0600

    Cuidado con la API

--- src/app/services/inversiones.service.ts
+++ src/app/services/inversiones.service.ts
@@ -4,7 +4,8 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class InversionesService {
-  urlBase:string = "https://api-cvwcbtm6xa-uc.a.run.app";
+  //urlBase:string = "https://api-cvwcbtm6xa-uc.a.run.app";
+  urlBase:string = "localhost:3000";
   constructor(public httpClient: HttpClient) {}
 
   getJSON(url: string) {
